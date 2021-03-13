6b3373f083585ad497bf3116c49ec9c86dc6047e
superlocox <superlox@hotmail.es>
2021-03-13 10:37:05 -0400

    ns

--- db.js
+++ db.js
@@ -14,7 +14,11 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 const proConfig = `postgres://ehegfleqnmhrbt:f4deb8f3274e7291c17225b39380648b3d7769337a0f461bc27d4bc459008f88@ec2-54-164-22-242.compute-1.amazonaws.com:5432/d7kp4krumesgkf`; //heroku addons
 console.log(proConfig);
 const pool = new Pool({
-  connectionString: proConfig
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
